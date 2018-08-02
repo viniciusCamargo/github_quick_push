9a7f147fd9da728519e2582f04fa4722cdf459ea
Vinicius Camargo <cmrg.vinicius@gmail.com>
2018-08-02 00:10:24 -0300

    init

---
+++ README.txt
@@ -0,0 +1,32 @@
+- Is the current directory a git repository?
+
+  - yes
+
+    - Show the detailed status of the repository
+    - Is it okay to push everything in it?
+    - What is the name of the repository? Default is the directory name.
+    - Push to Github
+    - Was the request OK?
+
+      - yes
+
+        - Open the created directory on the browser
+
+      - no
+        - Print the HTTP status code.
+
+  - no
+
+    - Create a new repository
+    - Show the detailed status of the repository
+    - Is it okay to push everything in it?
+    - What is the name of the repository? Default is the directory name.
+    - Push to Github
+    - Was the request OK?
+
+      - yes
+
+        - Open the created directory on the browser
+
+      - no
+        - Print the HTTP status code.
---
+++ main.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# GITHUB_TOKEN is set as an environment variable
+
+DIRNAME="$(basename $PWD)"
+USERNAME="viniciusCamargo"
+
+function create_repo_and_push {
+  git status --untracked-files
+
+  read -p "Push all? (Y/n): " confirm
+
+  if [[ "$confirm" = "n" || "$confirm" = "N" ]]; then
+    exit
+  fi
+
+  git add .
+  git commit -m "init"
+
+  read -p "Please, enter the repository name ($DIRNAME): " REPO_NAME
+
+  if [ "$REPO_NAME" = "" ]; then
+    REPO_NAME="$DIRNAME"
+  fi
+
+  STATUS_CODE=$(curl --write-out %{http_code} --silent --output /dev/null --header "Authorization: token $GITHUB_TOKEN" --data "{\"name\": \"${REPO_NAME}\", \"private\": false }" https://api.github.com/user/repos)
+  
+  if [ "$STATUS_CODE" = 201 ]; then
+    git remote add github git@github.com:${USERNAME}/${REPO_NAME}.git
+    git push -u github master
+
+    clear
+
+    xdg-open "https://github.com/$USERNAME/$REPO_NAME"
+  else
+    echo "####################################################"
+    echo "##################### nope $STATUS_CODE #####################"
+    echo "####################################################"
+  fi
+}
+
+if [ -d .git ]; then # if has a .git directory = equal git repo
+  create_repo_and_push
+else
+  git init
+  create_repo_and_push
+fi;
